c30be0bff702a3f8606e8a64dca5aaa60b71d589
toast38coza <info@38.co.za>
2017-03-01 16:05:44 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div>
+      Text: <input v-model='text' />
+      Percent: <input type='number' v-model='percent' />
+      Size:
+      <select v-model="selectedSize">
+        <option v-for="size in sizes" v-model="selectedSize">
+          {{ size }}
+        </option>
+      </select>
+      Color:
+      <select v-model="selectedColor">
+        <option v-for="color in colors" v-model="selectedColor">
+          {{ color }}
+        </option>
+      </select>
+      <hr/>
+    </div>
+    <center>
+    <vue-circle
+      :text='text'
+      :size='selectedSize'
+      :color='selectedColor'
+      :percent-complete='percentNumber' ></vue-circle>
+    </center>
+  </div>
+</template>
+
+<script>
+import VueCircle from './components/Circle'
+
+export default {
+  name: 'app',
+  components: {
+    VueCircle
+  },
+  data () {
+    return {
+      percent: 50,
+      sizes: ['big', 'medium', 'small'],
+      colors: ['default', 'green', 'orange'],
+      selectedSize: 'big',
+      selectedColor: 'default',
+      text: null
+    }
+  },
+  computed: {
+    percentNumber () {
+      return parseInt(this.percent)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
